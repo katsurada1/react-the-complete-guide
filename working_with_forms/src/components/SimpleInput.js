5b7675284f25ba2d8105f32aaa973194f65d6a42
kasturada1 <kazuki1.katsurada@gmail.com>
2023-05-19 20:51:24 +0900

    Add submit function

--- working_with_forms/src/components/SimpleInput.js
+++ working_with_forms/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnterdName] = useState('');
+
+  const nameInputChanegeHandler = (event) => {
+    setEnterdName(event.target.value);
+  };
+
+  const formSunmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    setEnterdName('');
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSunmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          onChange={nameInputChanegeHandler}
+          type="text"
+          id="name"
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
